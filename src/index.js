4197d8ba2b9d74e9bcdf8ad768abe4f5f18e514a
Vsevolod <lavrow.vs@gmail.com>
2020-10-03 11:31:22 +0300

    init: start task ,feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array) {  
+  if ( array && array.length ) {
+  var len = array.length;
+  var min = Infinity;
+    while(len--) {
+      if (array[len] < min) {
+        min = array[len];
+      }
+    }
+    return min;
+  }
+    else return 0;
+} 
 
 exports.max = function max (array) {
-  return 0;
+  if ( array && array.length ){
+  var len = array.length;
+  var max = -Infinity;
+    while(len--) {
+      if (array[len] > max) {
+        max = array[len];
+      }
+    }
+    return max;
+  }
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array && array.length ) {
+    var len = array.length;
+    var avg = 0;
+      while (len--) {
+        avg += array[len];
+      }
+      return avg / array.length;
+  } else return 0;
 }
